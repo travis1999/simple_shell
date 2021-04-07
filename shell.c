b7bb428bbf6df99d104edbc3a2ef95fbc0766531
travis1999 <trevoroguna@gmail.com>
2021-04-07 13:14:47 +0300

    Handled user input in interactive mode

---
+++ shell.c
@@ -0,0 +1,43 @@
+#include "tokenizer.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+
+size_t input(char **buffer, size_t *buffer_size)
+{
+	char *prompt = "($) ";
+	size_t size;
+
+	write(1, prompt, 4);
+	
+	size = getline(buffer, buffer_size, stdin);
+
+	return (size);
+}
+
+int main(int argc, char **argv)
+{
+	size_t buffer_size = 1024;
+	char *buffer = malloc(buffer_size);
+	size_t b_read;
+
+	Token *token_list = NULL;
+
+	while (1)
+	{
+		b_read = input(&buffer, &buffer_size);
+
+		if (b_read == -1)
+			break;
+
+		buffer[b_read - 1] = 0;
+		
+		if (!strcmp(buffer, "exit"))
+			break;
+		
+	}
+
+	free(buffer);
+	return (0);
+}
