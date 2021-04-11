2ba980ed14225b8b4bf94cd6e202b976cf8e8d0c
travis1999 <trevoroguna@gmail.com>
2021-04-11 12:51:00 +0300

    Added str utils

---
+++ strutils.h
@@ -0,0 +1,9 @@
+#ifndef STR_UTILS_H
+#define STR_UTILS_H
+
+int _putchar(char c);
+int str_comp(char *str0, char *str1);
+int str_print(char *str);
+int str_exists(char *str, char **str_list);
+
+#endif
