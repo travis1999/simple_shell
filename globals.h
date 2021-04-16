1d1d1ba0933d3595893f8f8f837211a7481c098a
travis1999 <trevoroguna@gmail.com>
2021-04-16 18:52:12 +0300

    Changed input function prototype

--- globals.h
+++ globals.h
@@ -1,8 +1,14 @@
 #ifndef GLOBALS
 #define GLOBALS
+
+#define true 1
+#define false 0
+
 #include <stddef.h>
 #include<stdio.h>
 
-size_t input(char **buffer, size_t *buffer_size);
+
+
+size_t input(char *buffer, int show_prompt);
 
 #endif
